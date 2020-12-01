3f4272c4684bdaffa23126fec25c8b94f7d8ed79
VD42 <vladislav.kochemaev@gmail.com>
2020-12-01 13:40:36 +0400

    For better testing

--- runner/run_without_strategy_vs_last.bat
+++ runner/run_without_strategy_vs_last.bat
@@ -1,4 +1,8 @@
 @echo off
 set /p last=<last.txt
 start aicup2020.exe --config config2.json
-..\strategy\x64\Release\AI20_%last%.exe 127.0.0.1 31002
+start ..\strategy\x64\Release\AI20_%last%.exe 127.0.0.1 31002
+set /a last=%last%-1
+start ..\strategy\x64\Release\AI20_%last%.exe 127.0.0.1 31003
+set /a last=%last%-1
+..\strategy\x64\Release\AI20_%last%.exe 127.0.0.1 31004
